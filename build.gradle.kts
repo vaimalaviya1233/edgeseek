e641b1a6b4221910f176d93c7222162a90213eba
Sulaiman Oboody <lsafer@cufy.org>
2025-11-19 17:24:47 +0300

    version 21 0.3-pre.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 //import org.jetbrains.kotlin.gradle.plugin.extraProperties
 
 group = "net.lsafer.edgeseek"
-version = "0.3-pre.6"
-project.ext["version_code"] = 20
+version = "0.3-pre.7"
+project.ext["version_code"] = 21
 project.ext["application_id"] = "lsafer.edgeseek"
 
 tasks.wrapper {
